948394824ccea0763de92e7d234c69ca222ae4cf
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-10-05 20:59:10 -0300

    :rotating_light: Fixed linter warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-900 p-24">
       <button type="button" className="btn">
         Button
       </button>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NavBar() {
+    return <nav className="left-0 top-0 w-full fixed  bg-red-500">teste</nav>;
+}
