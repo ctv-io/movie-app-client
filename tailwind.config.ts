6c44dfcb735e19c9c1eb28d6ee9c6a8d9865f16e
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-10-05 20:24:04 -0300

    :sparkles: Added cool af button xd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable global-require */
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('daisyui')],
+};
+export default config;
