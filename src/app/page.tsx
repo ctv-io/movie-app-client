3fbb4e7fbf8dc35713e6174b5fb047dae44e6db0
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-10-05 21:47:58 -0300

    :adhesive_bandage: Formatting + linter nonsense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-900 p-24">
-      <button type="button" className="btn">
-        Button
-      </button>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between bg-zinc-900 p-24">
+			<button type="button" className="btn">
+				Button
+			</button>
+		</main>
+	);
 }
