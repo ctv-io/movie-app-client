0b17a4061c7607f099861e1579445a8609ed9fc3
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-10-07 14:20:53 -0300

    :truck: Renamed Component for more discernment with future components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from '@/components/HeroSection';
+import MainPageHeroSection from '@/components/MainPageHeroSection';
 import NavBar from '@/components/NavBar';
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between bg-zinc-900 p-24">
 			<NavBar />
-			<HeroSection />
+			<MainPageHeroSection />
 		</main>
 	);
 }
